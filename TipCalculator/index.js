7af96a45e9c739e12c332b31fa7661f45bd9316a
sakshiigothwal <119726876+sakshiigothwal@users.noreply.github.com>
2025-06-02 16:41:54 +0530

    Updated

--- TipCalculator/index.js
+++ TipCalculator/index.js
@@ -2,11 +2,37 @@ const btn = document.getElementById("calculate");
 const billInput = document.getElementById("bill");
 const tipInput = document.getElementById("tip");
 const totalInput = document.getElementById("total");
+const errorMsg = document.getElementById("error");
+const clearAll = document.getElementById("clear");
 
 function calTotal(){
-    const billAmount = billInput.value;
-    const tipPercent = tipInput.value;
+    const billAmount = parseFloat(billInput.value);
+    const tipPercent = parseFloat(tipInput.value);
+
+    errorMsg.innerText= "";
+    totalInput.innerText= "";
+
+    if (isNaN(billAmount) || isNaN(tipPercent)) {
+        errorMsg.innerText = "Please enter valid amount or percentage.";
+        return;
+    }
+
+    if (billAmount < 0 || tipPercent < 0) {
+        errorMsg.innerText = "Negative values are not allowed.";
+        return;
+    }
+
     const totalAmount = Math.round(billAmount * (1 + tipPercent / 100));
-   totalInput.innerText = totalAmount;
+    totalInput.innerText = totalAmount;
+
 }
+
+function clearFields() {
+    billInput.value = "";
+    tipInput.value = "";
+    totalInput.innerText = "Total:";
+    errorMsg.innerText = "";
+}
+
 btn.addEventListener("click", calTotal);
+clearAll.addEventListener("click", clearFields);
